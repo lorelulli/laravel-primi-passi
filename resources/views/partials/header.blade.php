5a011db50edd366819d9af2bab9f8f9054ec30f5
lorenzo lulli <lullilore@gmail.com>
2020-05-06 01:49:50 +0200

    progressi

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="logo">
+        <img src="{{asset('img/logo.png')}}" alt="">
+
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a> </li>
+            <li><a href="#">Prodotti</a></li>
+            <li><a href="#">News</a></li>
+        </ul>
+        <div class="hamburger">
+            <i class="fas fa-bars"></i>
+
+        </div>
+    </nav>
+</header>

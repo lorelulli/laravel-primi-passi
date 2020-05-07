95d9648db67af39584caa657c52df0ffa58add3d
lorenzo lulli <lullilore@gmail.com>
2020-05-07 18:54:11 +0200

    accorgimenti vari

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -17,30 +17,31 @@ $card = $cards[$id];
             <div class="card-products">
                 <h2>{{$card['titolo']}}</h2>
 
-                <img src={{$card['src-p']}} alt="">
+                <div class="immagine">
+                    <img src={{$card['src-p']}} alt="">
+                    <div class="sinistra">
+                        @if ($id == 0)
+                            <a href="{{route('products.show', count($cards) - 1)}}"><i class="fas fa-chevron-left"></i></a>
+                        @else
+                            <a href="{{route('products.show', ($id - 1))}}"><i class="fas fa-chevron-left"></i></a>
+
+                        @endif
+
+                    </div>
+                    <div class="destra">
+
+                        @if ($id == (count($cards) - 1))
+                            <a href="{{route('products.show', 0)}}"><i class="fas fa-chevron-right"></i></a>
+
+                        @else
+                            <a href="{{route('products.show', ($id + 1))}}"><i class="fas fa-chevron-right"></i></a>
+
+                        @endif
+
+                    </div>
+
+                </div>
                 <p> {!!$card['descrizione']!!}</p>
-
-                <div class="sinistra">
-                    @if ($id == 0)
-                        <a href="{{route('products.show', count($cards) - 1)}}"><i class="fas fa-chevron-left"></i></a>
-                    @else
-                        <a href="{{route('products.show', ($id - 1))}}"><i class="fas fa-chevron-left"></i></a>
-
-                    @endif
-
-                </div>
-                <div class="destra">
-
-                    @if ($id == (count($cards) - 1))
-                        <a href="{{route('products.show', 0)}}"><i class="fas fa-chevron-right"></i></a>
-
-                    @else
-                        <a href="{{route('products.show', ($id + 1))}}"><i class="fas fa-chevron-right"></i></a>
-
-                    @endif
-
-                </div>
-
 
             </div>
 

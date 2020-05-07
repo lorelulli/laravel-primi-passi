4a542e4f36bf59d82113d20954d853eb30db9811
lorenzo lulli <lullilore@gmail.com>
2020-05-07 23:10:00 +0200

    footer abbozzato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,59 @@
+<footer>
+    <div class="up">
+        <div class="scheda prima">
+            <img src="{{asset('img/logo.png')}}" alt="">
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
+            <p>Ragione sociale: La Molisana Spa</p>
 
+        </div>
+        <div class="scheda seconda ">
+            <div class="pastificio">
+                <h3>PASTIFICIO</h3>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+
+            </div>
+            <div class="prodotti ">
+                <h3>PRODOTTI</h3>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+                <p>Il Pastificio</p>
+
+
+            </div>
+
+        </div>
+        <div class="scheda terza">
+            <h3>COLLEZIONE DA CHEF</h3>
+            <p>Collezione da Chef</p>
+            <p>Collezione da Chef</p>
+            <p>Collezione da Chef</p>
+            <p>Collezione da Chef</p>
+
+        </div>
+
+    </div>
+    <div class="down">
+        <img src="{{asset('img/montagne.jpg')}}" alt="">
+
+    </div>
+</footer>

ce99d913d22bfeccf6638f952f24b397695aaf08
lorenzo lulli <lullilore@gmail.com>
2020-05-19 02:38:28 +0200

    commit

---
+++ database/migrations/2020_05_18_165235_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',50);
+            $table->mediumText('src');
+            $table->mediumText('src-h');
+            $table->mediumText('src-p');
+            $table->tinyInteger('cottura');
+            $table->integer('peso');
+            $table->text('description');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
